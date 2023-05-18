c983287b9f6b1229a40ce6f43420160229041755
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-18 20:14:38 +0200

    added manifest.json

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -6,6 +6,6 @@
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <iframe :src="showPdf.url" type="application/pdf" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
+    <iframe :src="`https://drive.google.com/viewerng/viewer?embedded=true&url=${showPdf.url}`" type="application/pdf" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
   </div>
 </template>
