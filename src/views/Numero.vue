55565d22c42e7f0d68bffa20dcd90ce02b2137ee
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 13:35:49 +0200

    trying to fix pdf-to-base64

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -8,9 +8,7 @@
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
     <VuePdfEmbed :source="{cMapUrl: 'https://unpkg.com/pdfjs-dist/cmaps/',url: showPdf.url}"
-    image-resources-path="https://unpkg.com/pdfjs-dist/web/images/"
-    :disable-annotation-layer="true"
-    :disable-text-layer="true"/>
+    image-resources-path="https://unpkg.com/pdfjs-dist/web/images/"/>
     <!--<iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>-->
   </div>
 </template>
