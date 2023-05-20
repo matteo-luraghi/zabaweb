49ece2235094998f26c2bc5b075af5cbd7d000a8
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 13:15:21 +0200

    added vue-pdf-embed

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -8,7 +8,8 @@
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
     <VuePdfEmbed :source="showPdf.url"
-    image-resources-path="https://unpkg.com/pdfjs-dist/web/images/"/>
+    image-resources-path="https://unpkg.com/pdfjs-dist/web/images/"
+    :disable-annotation-layer="true"/>
     <!--<iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>-->
   </div>
 </template>
