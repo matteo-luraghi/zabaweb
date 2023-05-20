089562d5b03668b2c4f2b177493febf03da0edec
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 16:02:41 +0200

    hopless test #1000000

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
   import { showPdf } from '../state';
+  import { ref, onMounted } from 'vue';
+  const pdfSrc = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await fetch(showPdf.url);
+    const pdfData = await response.blob();
+    const dataUrl = URL.createObjectURL(pdfData);
+    pdfSrc.value = dataUrl;
+  } catch (error) {
+    console.error('Failed to load the PDF:', error);
+  }
+});
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <object :src="showPdf.url" type="application/pdf" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></object>
+    <iframe :src="pdfSrc" style="width:100%; height:1000px;"></iframe>
   </div>
 </template>
