629e33a69e1daf9e74045ef715a74f95439a328c
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 16:19:02 +0200

    resigning to pdf

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    // other plugins
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
   ]
 }
--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,34 +1,10 @@
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue';
-import { showPdf } from '../state';
-  import pdfjsLib from 'pdfjs-dist';
-  onMounted(async () => {
-  try {
-    const pdfResponse = await fetch(showPdf.url);
-    const pdfData = await pdfResponse.arrayBuffer();
-
-    const loadingTask = pdfjsLib.getDocument({ data: pdfData });
-    const pdf = await loadingTask.promise;
-
-    const canvas = ref<HTMLCanvasElement | null>(null);
-    const context = canvas.value?.getContext('2d');
-
-    const firstPage = await pdf.getPage(1);
-    const viewport = firstPage.getViewport({ scale: 1 });
-
-    canvas.value!.width = viewport.width;
-    canvas.value!.height = viewport.height;
-
-    await firstPage.render({ canvasContext: context!, viewport }).promise;
-  } catch (error) {
-    console.error('Failed to load the PDF:', error);
-  }
-})
+  import { showPdf } from '../state';
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <canvas ref="pdfCanvas"></canvas>
+    <iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
   </div>
 </template>
