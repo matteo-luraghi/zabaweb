eee51bc04a873d9fb502e06a8735e3543f441d2b
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 13:05:30 +0200

    added vue-pdf-embed

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import { showPdf } from '../state';
+  import VuePdfEmbed from 'vue-pdf-embed'
   //this view simply displays the pdf based on the global variable showPdf
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
+    <VuePdfEmbed :source="showPdf.url" width="100%"/>
+    <!--<iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>-->
   </div>
 </template>
