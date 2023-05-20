26a26998c2ad449bbd2063ade7f47f589d4d5d82
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 15:55:59 +0200

    hopless test #1000000

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,54 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { showPdf } from '../state';
-import pdfjsLib from 'pdfjs-dist';
-
-onMounted(() => {
-  const container = document.getElementById('pdf-viewer');
-  const pdfUrl = showPdf.url;
-
-  pdfjsLib.getDocument(pdfUrl).promise.then((pdf) => {
-    const numPages = pdf.numPages;
-
-    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-      pdf.getPage(pageNum).then((page) => {
-        const scale = 1.5;
-        const viewport = page.getViewport({ scale });
-
-        const canvas = document.createElement('canvas');
-        canvas.width = viewport.width;
-        canvas.height = viewport.height;
-
-        const context = canvas.getContext('2d');
-        if (!context) {
-          throw new Error('Failed to create 2D rendering context.');
-        }
-
-        const renderContext = {
-          canvasContext: context,
-          viewport,
-        };
-
-        page.render(renderContext).promise.then(() => {
-          container!.appendChild(canvas);
-        });
-      });
-    }
-  });
-});
+  import { showPdf } from '../state';
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <div id="pdf-viewer"></div>
+    <object :src="showPdf.url" type="application/pdf" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></object>
   </div>
 </template>
-
-<style>
-#pdf-viewer {
-  width: 100%;
-  height: 1000px;
-  zoom: calc(100vw / 900);
-}
-</style>
