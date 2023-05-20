10c6b54e3f712a064d649694f53b9e61c4b26d01
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 13:08:16 +0200

    added vue-pdf-embed

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -7,7 +7,7 @@
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <VuePdfEmbed :source="showPdf.url" width="100%"/>
+    <VuePdfEmbed :source="showPdf.url"/>
     <!--<iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>-->
   </div>
 </template>
