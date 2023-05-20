16b5c3c1d53135fa4cbcca2dd0c89d24bf292f98
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 15:30:06 +0200

    back to iframe

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
   import { showPdf } from '../state';
-  //this view simply displays the pdf based on the global variable showPdf
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <embed :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"/>
+    <iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
   </div>
 </template>
