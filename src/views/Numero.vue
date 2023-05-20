97979e56a1504e12d233919daf87c8c836d47a9b
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 15:49:26 +0200

    trying some chatgpt magic with embed

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
-  import { showPdf } from '../state';
+import { onMounted, ref } from 'vue';
+import { showPdf } from '../state';
+
+const pdfViewer = ref(null);
+
+onMounted(() => {
+  const container = document.getElementById('pdf-viewer');
+  const pdfUrl = showPdf.url;
+
+  const viewerUrl = '/path/to/pdf.js/web/viewer.html';
+  const viewerIframe = document.createElement('iframe');
+  viewerIframe.src = viewerUrl + '?file=' + encodeURIComponent(pdfUrl);
+  viewerIframe.width = '100%';
+  viewerIframe.height = '1000px';
+  viewerIframe.frameBorder = '0';
+
+  container!.appendChild(viewerIframe);
+});
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <embed :src="showPdf.url" type="application/pdf" style="width:100%; height:1000px; zoom: calc(100vw / 900)"/>
-    <iframe :src="showPdf.url" style="width:100%; height:1000px; zoom: calc(100vw / 900)" frameborder="0"></iframe>
+    <div id="pdf-viewer"></div>
   </div>
 </template>
+
+<style>
+#pdf-viewer {
+  width: 100%;
+  height: 1000px;
+  zoom: calc(100vw / 900);
+}
+</style>
