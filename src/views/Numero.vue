19d62968a9df6f5065e3964701b9ffd98978d2c6
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-20 16:11:00 +0200

    hopless test #1000000

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts">
-  import { showPdf } from '../state';
-  import { ref, onMounted } from 'vue';
-  const pdfSrc = ref('');
-
-onMounted(async () => {
+  import { onMounted, ref } from 'vue';
+import { showPdf } from '../state';
+  import pdfjsLib from 'pdfjs-dist';
+  onMounted(async () => {
   try {
-    const response = await fetch(showPdf.url);
-    const pdfData = await response.blob();
-    const dataUrl = URL.createObjectURL(pdfData);
-    pdfSrc.value = dataUrl;
+    const pdfResponse = await fetch(showPdf.url);
+    const pdfData = await pdfResponse.arrayBuffer();
+
+    const loadingTask = pdfjsLib.getDocument({ data: pdfData });
+    const pdf = await loadingTask.promise;
+
+    const canvas = ref<HTMLCanvasElement | null>(null);
+    const context = canvas.value?.getContext('2d');
+
+    const firstPage = await pdf.getPage(1);
+    const viewport = firstPage.getViewport({ scale: 1 });
+
+    canvas.value!.width = viewport.width;
+    canvas.value!.height = viewport.height;
+
+    await firstPage.render({ canvasContext: context!, viewport }).promise;
   } catch (error) {
     console.error('Failed to load the PDF:', error);
   }
-});
+})
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
-    <iframe :src="pdfSrc" style="width:100%; height:1000px;"></iframe>
+    <canvas ref="pdfCanvas"></canvas>
   </div>
 </template>
