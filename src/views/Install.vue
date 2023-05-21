d9797135d95510f1454f43e0da9f4b7b7d86a4b9
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-21 19:31:34 +0200

    trying to fix install

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,46 +1,81 @@
 <template>
   <div id="app">
-    <v-app>
-      <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
-        Get our free app. It won't take up space on your phone and also works
-        offline!
+    <div v-if="deferredPrompt" class="banner info text-left">
+      Get our free app. It won't take up space on your phone and also works
+      offline!
 
-        <template v-slot:actions>
-          <v-btn text @click="dismiss">Dismiss</v-btn>
-          <v-btn text @click="install">Install</v-btn>
-        </template>
-      </v-banner>
-      <div class="pa-4 text-center">
-        <h1>Customize Your Vue.js PWA Installation</h1>
+      <div class="actions">
+        <button @click="dismiss">Dismiss</button>
+        <button @click="install">Install</button>
       </div>
-    </v-app>
+    </div>
+    <div class="pa-4 text-center">
+      <h1>Customize Your Vue.js PWA Installation</h1>
+    </div>
   </div>
 </template>
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      deferredPrompt: null,
-    };
-  },
-  created() {
-    window.addEventListener("beforeinstallprompt", (e) => {
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-    });
-    window.addEventListener("appinstalled", () => {
-      this.deferredPrompt = null;
-    });
-  },
-  methods: {
-    async dismiss() {
-      this.deferredPrompt = null;
-    },
-    async install() {
-      this.deferredPrompt.prompt();
-    },
-  },
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<BeforeInstallPromptEventChoice>;
+  prompt(): Promise<void>;
+}
+
+interface BeforeInstallPromptEventChoice {
+  outcome: "accepted" | "dismissed";
+  platform: string;
+}
+
+let deferredPrompt = ref<BeforeInstallPromptEvent | null>(null);
+
+window.addEventListener("beforeinstallprompt", (e: Event) => {
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt.value = e as BeforeInstallPromptEvent;
+});
+
+window.addEventListener("appinstalled", () => {
+  deferredPrompt.value = null;
+});
+
+const dismiss = () => {
+  deferredPrompt.value = null;
+};
+
+const install = () => {
+  if (deferredPrompt.value) {
+    deferredPrompt.value.prompt();
+  }
 };
 </script>
+
+<style scoped>
+.banner {
+  color: white;
+  background-color: #2196f3;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.info {
+  background-color: #2196f3;
+}
+
+.text-left {
+  text-align: left;
+}
+
+.actions {
+  margin-top: 8px;
+}
+
+button {
+  margin-right: 8px;
+}
+</style>
