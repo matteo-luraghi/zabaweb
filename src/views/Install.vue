f1adfe45477826238e595c557d53c871ab2c6805
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-21 19:24:37 +0200

    trying to fix install

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,42 +1,46 @@
 <template>
-  <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
-    Get our free app. It won't take up space on your phone and also works
-    offline!
+  <div id="app">
+    <v-app>
+      <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
+        Get our free app. It won't take up space on your phone and also works
+        offline!
 
-    <template v-slot:actions>
-      <v-btn @click="dismiss">Dismiss</v-btn>
-      <v-btn @click="install">Install</v-btn>
-    </template>
-  </v-banner>
-  <div class="pa-4 text-center">
-    <h1>Customize Your Vue.js PWA Installation</h1>
+        <template v-slot:actions>
+          <v-btn text @click="dismiss">Dismiss</v-btn>
+          <v-btn text @click="install">Install</v-btn>
+        </template>
+      </v-banner>
+      <div class="pa-4 text-center">
+        <h1>Customize Your Vue.js PWA Installation</h1>
+      </div>
+    </v-app>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
-
-let deferredPrompt = ref<Event | null>(null);
-
-onMounted(() => {
-  window.addEventListener("beforeinstallprompt", (e: Event) => {
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt.value = e;
-  });
-
-  window.addEventListener("appinstalled", () => {
-    deferredPrompt.value = null;
-  });
-});
-
-const dismiss = () => {
-  deferredPrompt.value = null;
-};
-
-const install = () => {
-  if (deferredPrompt.value) {
-    (deferredPrompt.value as any).prompt();
-  }
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    },
+  },
 };
 </script>
