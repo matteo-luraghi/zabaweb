94f4f42cb3cef46656fb6983ec88b65361a19158
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-21 15:43:25 +0200

    trying to fix pdf

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -6,7 +6,7 @@ import { showPdf } from "../state";
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
     <iframe
-      :src="`https://drive.google.com/viewerng/viewer?embedded=true&url=${showPdf.url}`"
+      :src="`https://drive.google.com/viewerng/viewer?embedded=true&url=https://drive.google.com/file/d/13hCoWS_F9bEpmkK5M4_GrTkQkMn2Y6af/view?usp=sharing`"
       frameborder="0"
       id="myiframe"
       style="height: 100%; width: 100%"
