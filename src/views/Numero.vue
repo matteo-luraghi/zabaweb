0fc67d394f9cd09e23d206d91c5b1740707413bb
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-21 15:35:33 +0200

    trying to fix pdf

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -6,7 +6,7 @@ import { showPdf } from "../state";
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
     <iframe
-      :src="`http://docs.google.com/gview?url=${showPdf.url}.pdf&embedded=true`"
+      :src="`https://docs.google.com/gview?url=${showPdf.url}.pdf&embedded=true`"
       frameborder="0"
       id="myiframe"
       style="height: 100%; width: 100%"
