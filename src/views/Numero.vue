fad12707107cc01be0864eaa0524a4a4ab0e02ec
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-21 15:55:07 +0200

    trying to fix pdf

--- src/views/Numero.vue
+++ src/views/Numero.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts">
 import { showPdf } from "../state";
+let showUrl =
+  "https://drive.google.com/viewerng/viewer?embedded=true&url=" +
+  encodeURIComponent(showPdf.url);
 </script>
 
 <template>
   <div>
     <h2 class="text-font">{{ showPdf.name }}</h2>
     <iframe
-      :src="
-        'https://drive.google.com/viewerng/viewer?embedded=true&url=' +
-        encodeURIComponent(showPdf.url)
-      "
+      :src="showUrl"
       frameborder="0"
-      style="
-         {
-          height: 1000px;
-          width: 100%;
-        }
-      "
+      width="100%"
+      height="1000px"
     ></iframe>
   </div>
 </template>
