3ad696201364a0933960694921664c51a8f492cb
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-22 19:10:12 +0200

    reaching articles via link

--- src/views/Articolo.vue
+++ src/views/Articolo.vue
@@ -5,6 +5,8 @@ const props = defineProps<{
   query: string;
 }>();
 const articleId = parseInt(props.query.split("#")[0]);
+console.log(articleId);
+console.log(props.query.split("#"));
 //the global variable articleDetails contains all the info to display a single article
 //it gets updated by the Cards component
 let wideScreen = true;
