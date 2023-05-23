b5b0dc96fe53b7977603f3e71af9390e2e877503
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-23 16:34:54 +0200

    trying to add install button

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -5,27 +5,46 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 
-declare global {
-  interface Window {
-    beforeinstallprompt: Event;
-  }
-}
-
 const showDownloadButton = ref(false);
+let deferredPrompt: BeforeInstallPromptEvent | null = null;
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: "accepted" | "dismissed" }>;
+}
 
 onMounted(() => {
   // Check if the Web App Install Prompt API is supported and show the download button
   window.addEventListener("beforeinstallprompt", (event: Event) => {
     event.preventDefault();
+    deferredPrompt = event as BeforeInstallPromptEvent;
     showDownloadButton.value = true;
   });
 });
 
-const installApp = () => {
-  // Check if the Web App Install Prompt API is available
-  if ("beforeinstallprompt" in window) {
-    const installPromptEvent = new Event("beforeinstallprompt");
-    window.dispatchEvent(installPromptEvent);
+const installApp = async () => {
+  // Check if the Web App Install Prompt API is available and deferredPrompt is set
+  if (deferredPrompt) {
+    try {
+      // Hide the app-provided install promotion
+      hideInstallPromotion();
+      // Show the install prompt
+      await deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const { outcome } = await deferredPrompt.userChoice;
+      // Optionally, send analytics event with outcome of user choice
+      console.log(`User response to the install prompt: ${outcome}`);
+      // We've used the prompt, and can't use it again, throw it away
+      deferredPrompt = null;
+    } catch (error) {
+      console.error("Error during app installation:", error);
+    }
   }
 };
+
+function hideInstallPromotion() {
+  // Implement your logic to hide the install promotion UI
+  // For example, you can toggle a CSS class or manipulate the DOM
+  // based on your specific UI implementation
+}
 </script>
