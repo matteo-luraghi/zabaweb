66835cbfc4b3f5e7c75796a65399755b61ca4409
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-23 17:05:39 +0200

    trying to add install button

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,31 +1,40 @@
 <template>
-  <button @click="installApp">Download App</button>
+  <button @click="installApp" v-if="deferredPrompt">Download App</button>
 </template>
 
 <script setup lang="ts">
-let deferredPrompt: BeforeInstallPromptEvent;
+import { onMounted, ref } from "vue";
+
+let deferredPrompt: BeforeInstallPromptEvent | null = null;
 
 interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
   userChoice: Promise<{ outcome: "accepted" | "dismissed" }>;
 }
 
-// Check if the Web App Install Prompt API is supported and show the download button
-window.addEventListener("beforeinstallprompt", (event: Event) => {
-  event.preventDefault();
-  deferredPrompt = event as BeforeInstallPromptEvent;
+onMounted(() => {
+  // Check if the Web App Install Prompt API is supported and show the download button
+  window.addEventListener("beforeinstallprompt", (event: Event) => {
+    event.preventDefault();
+    deferredPrompt = event as BeforeInstallPromptEvent;
+  });
 });
 
 const installApp = async () => {
-  try {
-    // Show the install prompt
-    await deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    const { outcome } = await deferredPrompt.userChoice;
-    // Optionally, send analytics event with outcome of user choice
-    console.log(`User response to the install prompt: ${outcome}`);
-  } catch (error) {
-    console.error("Error during app installation:", error);
+  if (deferredPrompt) {
+    try {
+      // Show the install prompt
+      await deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const { outcome } = await deferredPrompt.userChoice;
+      // Optionally, send analytics event with outcome of user choice
+      console.log(`User response to the install prompt: ${outcome}`);
+    } catch (error) {
+      console.error("Error during app installation:", error);
+    } finally {
+      // Reset the deferredPrompt
+      deferredPrompt = null;
+    }
   }
 };
 </script>
