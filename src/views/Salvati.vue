f1131c9278be79f5f39b1cf5e3d35ab9f94f1000
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-24 23:18:05 +0200

    added saved view

--- src/views/Salvati.vue
+++ src/views/Salvati.vue
@@ -1,44 +1,14 @@
-<script setup lang="ts">
-import { computed, reactive, watch } from "vue";
-import { dataJson } from "@/state";
-
-const savedArticlesId = computed(() => {
-  const savedArticlesId = localStorage.getItem("SavedArticles");
-  return savedArticlesId ? JSON.parse(savedArticlesId) : [];
-});
-
-const savedArticlesData = computed(() => {
-  return savedArticlesId.value.map((articleId: number) => {
-    // Specify the type of 'articleId'
-    const index = dataJson.length - parseInt(articleId.toString()); // Convert 'articleId' to string before parsing
-    return dataJson[index];
-  });
-});
-
-const savedArticles = reactive({
-  articles: savedArticlesData.value,
-});
-
-watch(savedArticlesData, (newArticles) => {
-  savedArticles.articles = newArticles;
-});
-</script>
-
 <template>
   <h1 class="header articoli-title">SALVATI</h1>
-  <h3 class="title-font">Articoli</h3>
-  <div class="container">
-    <Card
-      v-for="article in savedArticles.articles"
-      :key="article.id"
-      :id="article.id"
-      :title="article.title"
-      :subtitle="article.subtitle"
-      :plaintext="article.plaintext"
-      :date="article.date"
-      :tags="article.tags"
-      :authors="article.authors"
-      :img="article.img"
-    />
-  </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const savedArticlesId = computed(() => {
+  const savedArticles = localStorage.getItem("SavedArticles");
+  return savedArticles ? JSON.parse(savedArticles) : [];
+});
+
+console.log(savedArticlesId.value);
+</script>
