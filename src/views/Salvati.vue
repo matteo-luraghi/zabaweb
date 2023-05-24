3fe29ef95cbace3ea645eb6647a2bfc661b0d4e4
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-24 22:51:08 +0200

    added saved view

--- src/views/Salvati.vue
+++ src/views/Salvati.vue
@@ -1,28 +1,38 @@
 <template>
   <h1 class="header articoli-title">SALVATI</h1>
+  <h3 class="title-font">Articoli</h3>
+  <div class="container">
+    <Card
+      v-if="savedArticles.length != 0"
+      v-for="page in savedArticles"
+      :id="page.id"
+      :title="page.title"
+      :subtitle="page.subtitle"
+      :plaintext="page.plaintext"
+      :date="page.date"
+      :tags="page.tags"
+      :authors="page.authors"
+      :img="page.img"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import { dataJson } from "@/state";
 
-const savedArticlesId = computed(() => {
-  const savedArticles = localStorage.getItem("SavedArticles");
-  return savedArticles ? JSON.parse(savedArticles) : [];
-});
-
-for (let i in savedArticlesId.value) {
-  console.log(
-    dataJson[
-      (dataJson.length - parseInt(savedArticlesId.value[i])) as keyof object
-    ]
-  );
-  console.log(savedArticlesId.value[i]);
-}
-
 const savedArticles = computed(() => {
-  for (let i in savedArticlesId.value) {
-    console.log(dataJson[i as keyof object]);
+  let savedArticles = [];
+  const savedArticlesStorage = localStorage.getItem("SavedArticles");
+  const saveArticlesIdData = savedArticlesStorage
+    ? JSON.parse(savedArticlesStorage)
+    : [];
+  let savedArticlesId: string[] = saveArticlesIdData.value;
+  for (let i in savedArticlesId) {
+    savedArticles.push(
+      dataJson[(dataJson.length - parseInt(savedArticlesId[i])) as keyof object]
+    );
   }
+  return savedArticles;
 });
 </script>
