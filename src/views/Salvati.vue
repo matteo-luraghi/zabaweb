675d7cbf8eeaeec5fcdba20f9187901c5a3937ba
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-24 23:15:54 +0200

    added saved view

--- src/views/Salvati.vue
+++ src/views/Salvati.vue
@@ -1,24 +1,5 @@
-<template>
-  <h1 class="header articoli-title">SALVATI</h1>
-  <h3 class="title-font">Articoli</h3>
-  <div class="container">
-    <Card
-      v-if="savedArticles.articles.length != 0"
-      v-for="page in savedArticles.articles"
-      :id="page.id"
-      :title="page.title"
-      :subtitle="page.subtitle"
-      :plaintext="page.plaintext"
-      :date="page.date"
-      :tags="page.tags"
-      :authors="page.authors"
-      :img="page.img"
-    />
-  </div>
-</template>
-
 <script setup lang="ts">
-import { computed, reactive } from "vue";
+import { computed, reactive, watch } from "vue";
 import { dataJson } from "@/state";
 
 const savedArticlesId = computed(() => {
@@ -26,19 +7,38 @@ const savedArticlesId = computed(() => {
   return savedArticlesId ? JSON.parse(savedArticlesId) : [];
 });
 
-let savedArticlesData = computed(() => {
-  let savedArticlesData = [];
-  for (let i in savedArticlesId.value) {
-    savedArticlesData.push(
-      dataJson[dataJson.length - parseInt(savedArticlesId.value[i])]
-    );
-  }
-  return savedArticlesData;
+const savedArticlesData = computed(() => {
+  return savedArticlesId.value.map((articleId: number) => {
+    // Specify the type of 'articleId'
+    const index = dataJson.length - parseInt(articleId.toString()); // Convert 'articleId' to string before parsing
+    return dataJson[index];
+  });
 });
 
-let savedArticles = reactive({
+const savedArticles = reactive({
   articles: savedArticlesData.value,
 });
 
-console.log(savedArticles.articles);
+watch(savedArticlesData, (newArticles) => {
+  savedArticles.articles = newArticles;
+});
 </script>
+
+<template>
+  <h1 class="header articoli-title">SALVATI</h1>
+  <h3 class="title-font">Articoli</h3>
+  <div class="container">
+    <Card
+      v-for="article in savedArticles.articles"
+      :key="article.id"
+      :id="article.id"
+      :title="article.title"
+      :subtitle="article.subtitle"
+      :plaintext="article.plaintext"
+      :date="article.date"
+      :tags="article.tags"
+      :authors="article.authors"
+      :img="article.img"
+    />
+  </div>
+</template>
