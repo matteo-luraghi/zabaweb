1f8bd365cab46b9c9032b9c8d58a61f91c3003f1
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-25 08:42:05 +0200

    added saved view

--- src/views/Salvati.vue
+++ src/views/Salvati.vue
@@ -1,8 +1,25 @@
 <template>
   <h1 class="header articoli-title">SALVATI</h1>
+  <h3 class="title-font">Articoli</h3>
+
+  <div class="container">
+    <Card
+      v-if="savedArticles.length != 0"
+      v-for="page in savedArticles"
+      :id="page.id"
+      :title="page.title"
+      :subtitle="page.subtitle"
+      :plaintext="page.plaintext"
+      :date="page.date"
+      :tags="page.tags"
+      :authors="page.authors"
+      :img="page.img"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { dataJson } from "@/state";
 import { computed } from "vue";
 
 const savedArticlesId = computed(() => {
@@ -10,5 +27,19 @@ const savedArticlesId = computed(() => {
   return savedArticles ? JSON.parse(savedArticles) : [];
 });
 
-console.log(savedArticlesId.value);
+let savedArticles: {
+  id: number;
+  title: string;
+  text: {};
+  plaintext: string;
+  subtitle: string;
+  date: string;
+  tags: string[];
+  authors: string[];
+  img: string;
+}[] = [];
+
+for (let i in savedArticlesId.value) {
+  savedArticles.push(dataJson[dataJson.length - savedArticlesId.value[i]]);
+}
 </script>
