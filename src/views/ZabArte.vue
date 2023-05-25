15a53a79a1790fe1d5d6096d235f86c68c5d8dac
matteo-luraghi <matteoluraghi50@gmail.com>
2023-05-25 11:49:12 +0200

    trying to add swipe input

--- src/views/ZabArte.vue
+++ src/views/ZabArte.vue
@@ -1,10 +1,13 @@
 <template>
   <h1 class="header articoli-title">ZABARTE</h1>
-  <div class="image-container">
+  <div
+    class="image-container"
+    @touchstart="handleTouchStart"
+    @touchend="handleTouchEnd"
+  >
     <img
       :src="currentImage.img"
       alt="Zabarte Image"
-      @click="showNextImage"
       :class="{ swipe: isSwiped }"
     />
   </div>
@@ -33,13 +36,38 @@ const zabarte = [
 
 const currentIndex = ref(0);
 const isSwiped = ref(false);
+const touchStartX = ref(0);
 
 const currentImage = computed(() => zabarte[currentIndex.value]);
+
+const handleTouchStart = (event: TouchEvent) => {
+  touchStartX.value = event.touches[0].clientX;
+};
+
+const handleTouchEnd = (event: TouchEvent) => {
+  const touchEndX = event.changedTouches[0].clientX;
+  const deltaX = touchEndX - touchStartX.value;
+
+  if (deltaX > 50) {
+    showPrevImage();
+  } else if (deltaX < -50) {
+    showNextImage();
+  }
+};
 
 const showNextImage = () => {
   isSwiped.value = true;
   setTimeout(() => {
     currentIndex.value = (currentIndex.value + 1) % zabarte.length;
+    isSwiped.value = false;
+  }, 500);
+};
+
+const showPrevImage = () => {
+  isSwiped.value = true;
+  setTimeout(() => {
+    currentIndex.value =
+      (currentIndex.value - 1 + zabarte.length) % zabarte.length;
     isSwiped.value = false;
   }, 500);
 };
@@ -57,6 +85,5 @@ img {
 
 .swipe {
   transition: transform 0.5s ease-in-out;
-  transform: translateX(100%);
 }
 </style>
