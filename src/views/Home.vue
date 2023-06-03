28af9f1ab9561734c8c82684f8af98f327acbeaa
matteo-luraghi <matteoluraghi50@gmail.com>
2023-06-03 20:53:41 +0200

    trying to fix crazy error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,15 +10,6 @@ import {
 //the variable dataJson stores all the info, then in the template via the v-for method are displayed the first 9 articles of the database
 
 //function to update and reload the database if the images can't be displyed due to notion changing the link
-async function updateAndReloadArchive() {
-  await updateArchiveDatabase();
-  window.location.reload();
-}
-
-async function updateAndReloadArt() {
-  await updateArtDatabase();
-  window.location.reload;
-}
 </script>
 
 <template>
@@ -50,12 +41,7 @@ async function updateAndReloadArt() {
       class="router-link"
     >
       <h3 class="text-font">{{ archive[0].name }}</h3>
-      <img
-        alt="numero-cover"
-        class="numero-cover"
-        :src="archive[0].img"
-        @error="updateAndReloadArchive"
-      />
+      <img alt="numero-cover" class="numero-cover" :src="archive[0].img" />
     </router-link>
   </div>
 
@@ -65,12 +51,7 @@ async function updateAndReloadArt() {
       :to="`/zabarte/view?q=${zabarte[0].id}#${zabarte[0].title}`"
       class="router-link"
     >
-      <img
-        :src="zabarte[0].img"
-        alt="Zabarte Image"
-        @error="updateAndReloadArt"
-        class="responsive-image"
-      />
+      <img :src="zabarte[0].img" alt="Zabarte Image" class="responsive-image" />
     </router-link>
   </div>
 </template>
