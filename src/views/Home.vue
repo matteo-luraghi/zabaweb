c803a650a83fb9958db18962dbda1a3882492a7b
matteo-luraghi <matteoluraghi50@gmail.com>
2023-06-03 09:17:46 +0200

    timeout of 1000 and home changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,58 +22,56 @@ async function updateAndReloadArt() {
 </script>
 
 <template>
+  <h1 class="header articoli-title">HOME</h1>
   <div class="container">
-    <h1 class="header articoli-title">HOME</h1>
-    <div class="container">
+    <img
+      alt="testata"
+      class="testata"
+      src="../assets/testata.webp"
+      style="max-width: 100%"
+    />
+    <h3 class="text-font home-title">Ultimo articolo</h3>
+    <Card
+      :id="dataJson[0].id"
+      :title="dataJson[0].title"
+      :plaintext="dataJson[0].plaintext"
+      :subtitle="dataJson[0].subtitle"
+      :date="dataJson[0].date"
+      :tags="dataJson[0].tags"
+      :authors="dataJson[0].authors"
+      :img="dataJson[0].img"
+    />
+  </div>
+
+  <h3 class="text-font home-title">Ultimo numero</h3>
+  <div class="container">
+    <router-link
+      :to="`/numero?q=${archive[0].id}#${archive[0].name}`"
+      class="router-link"
+    >
+      <h3 class="text-font">{{ archive[0].name }}</h3>
       <img
-        alt="testata"
-        class="testata"
-        src="../assets/testata.webp"
-        style="max-width: 100%"
+        alt="numero-cover"
+        class="numero-cover"
+        :src="archive[0].img"
+        @error="updateAndReloadArchive"
       />
-      <h3 class="text-font home-title">Ultimo articolo</h3>
-      <Card
-        :id="dataJson[0].id"
-        :title="dataJson[0].title"
-        :plaintext="dataJson[0].plaintext"
-        :subtitle="dataJson[0].subtitle"
-        :date="dataJson[0].date"
-        :tags="dataJson[0].tags"
-        :authors="dataJson[0].authors"
-        :img="dataJson[0].img"
+    </router-link>
+  </div>
+
+  <h3 class="text-font home-title">Ultima uscita di Zabarte</h3>
+  <div class="container">
+    <router-link
+      :to="`/zabarte/view?q=${zabarte[0].id}#${zabarte[0].title}`"
+      class="router-link"
+    >
+      <img
+        :src="zabarte[0].img"
+        alt="Zabarte Image"
+        @error="updateAndReloadArt"
+        class="responsive-image"
       />
-    </div>
-
-    <h3 class="text-font home-title">Ultimo numero</h3>
-    <div class="container">
-      <router-link
-        :to="`/numero?q=${archive[0].id}#${archive[0].name}`"
-        class="router-link"
-      >
-        <h3 class="text-font">{{ archive[0].name }}</h3>
-        <img
-          alt="numero-cover"
-          class="numero-cover"
-          :src="archive[0].img"
-          @error="updateAndReloadArchive"
-        />
-      </router-link>
-    </div>
-
-    <h3 class="text-font home-title">Ultima uscita di Zabarte</h3>
-    <div class="container">
-      <router-link
-        :to="`/zabarte/view?q=${zabarte[0].id}#${zabarte[0].title}`"
-        class="router-link"
-      >
-        <img
-          :src="zabarte[0].img"
-          alt="Zabarte Image"
-          @error="updateAndReloadArt"
-          class="responsive-image"
-        />
-      </router-link>
-    </div>
+    </router-link>
   </div>
 </template>
 
