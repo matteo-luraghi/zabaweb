9d310980866c22ccfc02c2bbcd53f01ad4b79892
matteo-luraghi <matteoluraghi50@gmail.com>
2023-06-04 14:57:53 +0200

    trying to add loading animation

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -11,15 +11,9 @@ import Home from "./Home.vue";
 import Spinner from "./Spinner.vue";
 import { computed } from "vue";
 
-console.log(dataJson[0].id);
 await updateArticleDatabase();
-console.log(dataJson[0].id);
-console.log(archive[0].id);
 await updateArchiveDatabase();
-console.log(archive[0].id);
-console.log(zabarte[0].id);
 await updateArtDatabase();
-console.log(zabarte[0].id);
 
 let notReady = computed(() => {
   if (dataJson[0].id === -1 || archive[0].id === -1 || zabarte[0].id === -1) {
@@ -38,6 +32,6 @@ let notReady = computed(() => {
     src="../assets/testata.webp"
     style="max-width: 100%"
   />
-  <Spinner v-if="notReady" />
+  <Spinner v-if="notReady === true" />
   <Home v-else />
 </template>
